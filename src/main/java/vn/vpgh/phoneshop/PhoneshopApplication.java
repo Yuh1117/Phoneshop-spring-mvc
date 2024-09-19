a3e576a7496061d0c7e7c84a63e8dc16783aad46
Yuh <huypham3188@gmail.com>
2024-09-19 17:44:37 +0700

    disable security

--- src/main/java/vn/vpgh/phoneshop/PhoneshopApplication.java
+++ src/main/java/vn/vpgh/phoneshop/PhoneshopApplication.java
@@ -3,9 +3,9 @@ package vn.vpgh.phoneshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class PhoneshopApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PhoneshopApplication.class, args);
     }
